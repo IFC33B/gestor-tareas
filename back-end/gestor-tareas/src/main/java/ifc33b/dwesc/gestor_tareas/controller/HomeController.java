02c530463915fc74a20fa4a4630014bafc426a6d
evivesheyne <evivesheyne@cifpfbmoll.eu>
2025-12-17 19:50:19 +0100

    Update HomeController.java

--- back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/controller/HomeController.java
+++ back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/controller/HomeController.java
@@ -1,28 +1,47 @@
 package ifc33b.dwesc.gestor_tareas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import ifc33b.dwesc.gestor_tareas.model.Tasca;
+import ifc33b.dwesc.gestor_tareas.service.TascaService;
+
+// @Autowired()
+@RestController
+@RequestMapping("/api")
 public class HomeController {
 
-    @GetMapping("/api/tasques") // Devolver todas las tareas
-    public String getApiTasques() {
-        return null;
+    @Autowired
+    private TascaService tascaService;
+
+    @GetMapping("/test")
+    public String getApiTest() {
+        return new Tasca("test", "test").toString();
     }
 
-    @PostMapping("/api/tasques") // Crear tarea
+    @GetMapping("/tasques") // Devolver todas las tareas
+    public List<Tasca> getApiTasques() {
+        return tascaService.getAllTasques();
+    }
+
+    @PostMapping("/tasques") // Crear tarea
     public String postApiTasques() {
         return null;
     }
 
-    @PutMapping("/api/tasques/{id}") // Editar tarea (Estado, titulo...)
+    @PutMapping("/tasques/{id}") // Editar tarea (Estado, titulo...)
     public String putApiTasques() {
         return null;
     }
 
-    @DeleteMapping("/api/tasques/{id}") // Elimina una tarea
+    @DeleteMapping("/tasques/{id}") // Elimina una tarea
     public String deleteApiTasques() {
         return null;
     }
