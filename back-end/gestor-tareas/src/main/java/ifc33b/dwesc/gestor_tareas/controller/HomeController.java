258c5aa7a3bfe517f7772884070941d2bd0a8f3b
evivesheyne <evivesheyne@cifpfbmoll.eu>
2025-12-18 11:22:39 +0100

    Update HomeController.java
    Add Edit path

--- back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/controller/HomeController.java
+++ back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,8 +56,12 @@ public class HomeController {
     }
 
     @PutMapping("/tasques/{id}") // Editar tarea (Estado, titulo...)
-    public String putApiTasques() {
-        return null;
+    public ResponseEntity<TascaResponse> putApiTasques(@RequestBody TascaRequest tascaRequest, @PathVariable Long id) {
+        // Service Logic
+        TascaResponse tascaResponse = tascaService.updateTasca(id, tascaRequest);
+
+        // Http Response
+        return new ResponseEntity<>(tascaResponse, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/tasques/{id}") // Elimina una tarea
