9cfe8461a791085620d3a3799138f94a7c6caa8d
evivesheyne <evivesheyne@cifpfbmoll.eu>
2025-12-18 11:23:05 +0100

    Update TascaRequest.java
    Now feta must be send

--- back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/dto/TascaRequest.java
+++ back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/dto/TascaRequest.java
@@ -12,12 +12,14 @@ public class TascaRequest {
     @NotBlank(message = "Es requereis una descripció per la tasca")
     private String descripcio;
 
+    private boolean feta;
+
     // Default Constructor
     public TascaRequest() {
     }
 
     // Constructor
-    public TascaRequest(String titol, String descripcio) {
+    public TascaRequest(String titol, String descripcio, boolean feta) {
         this.titol = titol;
         this.descripcio = descripcio;
     }
