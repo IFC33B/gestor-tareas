14fc25e37f126d720a201a560f0bdd253330b062
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-18 17:43:50 +0100

    Add - llista-tasques.ts: carregarTasques()

--- front-end/gestor-tareas/src/app/components/llista-tasques/llista-tasques.ts
+++ front-end/gestor-tareas/src/app/components/llista-tasques/llista-tasques.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Tasca } from '../../models';
+import { TascaService } from '../../services/tasca.service';
 
 @Component({
   selector: 'app-llista-tasques',
@@ -7,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './llista-tasques.scss',
 })
 export class LlistaTasques {
+  tasques = signal<Tasca[]>([]);
+  carregant = signal(true);
+  error = signal<string | null>(null);
 
+  constructor(private tascaService: TascaService){}
+
+  ngOnInit(): void {
+    this.carregarTasques();
+  }
+
+  // Cargar tasques
+  carregarTasques(): void {
+    this.carregant.set(true);
+    this.error.set(null);
+
+    this.tascaService.getAllTasques().subscribe({
+      next: (dades) => {
+        this.tasques.set(dades);
+        this.carregant.set(false);
+      },
+
+      error: (err) => {
+        this.error.set('Error al cargar las tareas');
+        this.carregant.set(false);
+        console.log(err);
+      }
+    })
+  }
 }
