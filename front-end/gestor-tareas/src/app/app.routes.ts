90302c025e1fe45485c0f3e9c2d15272881f4eb5
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-18 17:53:47 +0100

    Add - Routes

--- front-end/gestor-tareas/src/app/app.routes.ts
+++ front-end/gestor-tareas/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LlistaTasques } from './components/llista-tasques/llista-tasques';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/tasques', pathMatch: 'full'},
+    {path: 'tasques', component: LlistaTasques}
+];
