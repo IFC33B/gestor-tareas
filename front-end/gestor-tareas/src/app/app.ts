6541db36af1467c3284b08422936586e0ac483f9
evivesheyne <evivesheyne@cifpfbmoll.eu>
2025-12-19 16:22:07 +0100

    Front end structured

--- front-end/gestor-tareas/src/app/app.ts
+++ front-end/gestor-tareas/src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('gestor-tareas');
+  protected readonly title = signal('Pràctica 1 API - Client & Servidor');
 }
---
+++ front-end/gestor-tareas/src/app/components/llista-tasques/llista-tasques.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-llista-tasques',
+  imports: [],
+  templateUrl: './llista-tasques.html',
+  styleUrl: './llista-tasques.scss',
+})
+export class LlistaTasques {
+
+}
