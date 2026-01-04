5e071a4744c4e211702ca509723ef9ec9e64452f
Edwin Espinoza <edwin.smx2@gmail.com>
2026-01-04 17:16:41 +0100

    Modelo Tasca corregido

--- back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/model/Tasca.java
+++ back-end/gestor-tareas/src/main/java/ifc33b/dwesc/gestor_tareas/model/Tasca.java
@@ -1,32 +1,45 @@
+package ifc33b.dwesc.gestor_tareas.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "tasca")
 public class Tasca {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(nullable = false)
     private String titol;
+
+    @NotBlank
+    @Column(nullable = false)
     private String descripcio;
+
+    @Column(nullable = false)
     private boolean feta;
 
-    // constructor
+    // Constructor vacío
     public Tasca() {}
 
-    // constructor con todos los parametros
-    public Tasca(String titol, String descripcio, boolean feta) {
+    // Constructor para crear tareas nuevas
+    public Tasca(String titol, String descripcio) {
         this.titol = titol;
         this.descripcio = descripcio;
-        this.feta = feta;
+        this.feta = false;
     }
 
     // getters y setters
     public Long getId() {
         return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
     }
     public String getTitol() {
         return titol;
