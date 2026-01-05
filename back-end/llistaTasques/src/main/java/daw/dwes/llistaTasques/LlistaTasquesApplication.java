065bb337999b1e9eef2726ec6b9edbc202c64bbb
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-05 17:35:59 +0100

    Archivos incluidos en la creación del proyecto

---
+++ back-end/llistaTasques/src/main/java/daw/dwes/llistaTasques/LlistaTasquesApplication.java
@@ -0,0 +1,13 @@
+package daw.dwes.llistaTasques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlistaTasquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlistaTasquesApplication.class, args);
+	}
+
+}
