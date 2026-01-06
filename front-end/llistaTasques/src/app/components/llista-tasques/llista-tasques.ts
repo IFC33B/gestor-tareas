f102438bfa93b9423eec666f5a153f4f76c2cd12
Luis-Pozo-Benz <lpozobenz@cifpfbmoll.eu>
2026-01-06 13:56:25 +0100

    Componente llistaTasques

---
+++ front-end/llistaTasques/src/app/components/llista-tasques/llista-tasques.ts
@@ -0,0 +1,31 @@
+import { Component, signal } from '@angular/core';
+import { Tasca } from '../../models';
+import { TascaService } from '../../services/tasca.service';
+
+@Component({
+  selector: 'app-llista-tasques',
+  imports: [],
+  templateUrl: './llista-tasques.html',
+  styleUrl: './llista-tasques.scss',
+})
+export class LlistaTasques {
+
+  tasques = signal<Tasca[]>([]);
+  cargando = signal(true);
+  constructor(private tascaService:TascaService) {
+
+  }
+  ngOnInit():void {
+    this.cargarTasques();
+  }
+  cargarTasques():void {
+    this.cargando.set(true);
+    this.tascaService.getTasques().subscribe({
+      next:(data)=>{
+        this.tasques.set(data);
+        this.cargando.set(false);
+      }
+    })
+  }
+
+}
