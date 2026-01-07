8b57b3a381b427021f18ec49e75cee2e2eccd64a
Edwin Espinoza <edwin.smx2@gmail.com>
2026-01-07 19:04:58 +0100

    Añadido formulario para crear tarea

--- front-end/gestor-tareas/src/app/components/tasques/tasques.ts
+++ front-end/gestor-tareas/src/app/components/tasques/tasques.ts
@@ -1,28 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TascaService } from '../../services/tasca.service';
 import { Tasca } from '../../models/tasca.model';
 
 @Component({
   selector: 'app-tasques',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tasques.html',
   styleUrl: './tasques.scss',
 })
 export class Tasques implements OnInit {
   tasques: Tasca[] = [];
 
+  // guardar datos del formulario
+  novaTasca: Tasca = {
+    titol: '',
+    descripcio: '',
+    feta: false
+  };
+
+
   constructor(private tascaService: TascaService) {}
 
   ngOnInit(): void {
-    this.CargarTasques();
+    this.cargarTasques();
   }
 
-  CargarTasques(): void {
+  // mostrar lista de tareas
+  cargarTasques(): void {
     this.tascaService.getTasques().subscribe({
       next: (data) => this.tasques = data,
       error: (err) => console.error('Error cargando tareas', err)
+    });
+  }
+
+  // crear tarea
+  crearTasca(): void {
+    this.tascaService.createTasca(this.novaTasca).subscribe({
+      next: () => {
+        this.cargarTasques();
+        this.novaTasca = { titol: '', descripcio: '', feta: false };
+      },
+      error: err => console.error('Error creando tarea', err)
     });
   }
 }
