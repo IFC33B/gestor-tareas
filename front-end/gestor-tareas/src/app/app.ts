be5677e9970cb91175b40c1a693a434dcbc55423
Edwin Espinoza <edwin.smx2@gmail.com>
2026-01-07 14:55:18 +0100

    Importado componente Tasques en app

--- front-end/gestor-tareas/src/app/app.ts
+++ front-end/gestor-tareas/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Tasques } from './components/tasques/tasques';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Tasques
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
