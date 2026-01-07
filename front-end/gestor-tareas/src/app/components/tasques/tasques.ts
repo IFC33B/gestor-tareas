3c375570d51f4d9ec7a8ee06a4fde0a0622606dc
Edwin Espinoza <edwin.smx2@gmail.com>
2026-01-07 13:59:34 +0100

    Generado componente Tareas para ver lista de tareas

---
+++ front-end/gestor-tareas/src/app/components/tasques/tasques.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasques',
+  imports: [],
+  templateUrl: './tasques.html',
+  styleUrl: './tasques.scss',
+})
+export class Tasques {
+
+}
